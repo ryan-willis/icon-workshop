81e59322c85ac672f615c53741e60b19d241251a
Ryan Willis <code@ryanwillis.com>
2024-05-16 07:52:13 -0700

    add optional header tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { readFileSync } from "fs";
+try {
+  (readFileSync(".env.production")
+    .toString()
+    .match(/VITE_HEADER_TAGS/)
+    ? () => void 0
+    : () => {
+        throw new Error("");
+      })();
+} catch (e) {
+  process.env.VITE_HEADER_TAGS = "";
+}
 export default defineConfig({
   plugins: [react()],
 });
